6bbd401b9b40766f50ce119c08d5989a17ded0e7
Jon Dehdari <jon@dehdari.org>
2016-03-20 15:50:40 +0100

    m: word2int.py++

--- word2int.py
+++ word2int.py
@@ -6,6 +6,12 @@
 
 import sys
 from array import array
+import argparse
+parser = argparse.ArgumentParser(description='Converts words to integers, online')
+
+parser.add_argument('-f', '--file', help="Vocab file.  If the file exists, use it to map words to integers. If the file doesn't exist, save mapping to this file")
+args = parser.parse_args()
+
 
 words = {}
 count = 1
@@ -21,4 +27,4 @@ for line in sys.stdin:
             ints[w]  = count
             words[ws[w]] = count
             count    += 1
-    print(*ints, sep=' ')
+    print(*ints, sep=' ') # Use Python 3 interpreter
